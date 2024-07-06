6e4c6f6a9a828eb069e5ca0df6c02c66dd260b2b
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-07-06 22:44:38 +0530

    updated user model with password encryption logic

--- root/Backend/Models/User.models.js
+++ root/Backend/Models/User.models.js
@@ -1,29 +1,37 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username:{
         type: String,
         required: [true, "username is required"],
         lowercase: true,
         unique: true,
         trim:true,
-        minLength: 6,
-        maxLength: 32,
     },
     password:{
         type: String,
         required: [true, "Password is required"],
-        minLength: 8,
-        maxLength: 32,
     },
     email:{
         type: String,
-        required: true,
+        required: [true,"Email is required"],
         unique: true,
         lowecase: true,
         trim: true,
-        maxLength: 32,
     }
 },{timestamps:true});
 
-export const User = mongoose.model("User", UserSchema);
+
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+export const User = mongoose.model("User", userSchema);
