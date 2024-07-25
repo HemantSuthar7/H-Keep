8d10b940e268c5ce0e7c6c077c0d26555d1b0472
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-07-25 20:31:39 +0530

    added some utilities

--- root/Backend/controllers/user.controller.js
+++ root/Backend/controllers/user.controller.js
@@ -1,1 +1,22 @@
 // User related Logic
+
+import User from "../Models/User.models.js"
+
+
+/* SOME METHODS TO WRITE :
+
+    1. RegisterUser
+    2. Login
+    3. Logout // testing -- make sure the refresh token is getting deleted from db
+    4. Change current password - if the user knows the old password
+    5. Forgot password
+    6. refresh access token 
+    7. get current user
+    8. update account details
+    9. add more methods as per requirement
+
+*/
+
+const RegisterUser = async (req, res) => {
+    
+}
