6a2334346de6cb2c35ac700a23b9c3a082605fd0
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-08-05 13:11:55 +0530

    implemented login functionality with methods to genreate tokens

--- root/Backend/Models/User.models.js
+++ root/Backend/Models/User.models.js
@@ -1,5 +1,6 @@
 import mongoose , {Schema} from "mongoose";
 import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema({
     username:{
@@ -25,7 +26,7 @@ const userSchema = new Schema({
     // think about fullname
     fullName: {
         type: String,
-        required: [true,"Email is required"],
+        required: [true,"Fullname is required"],
         trim: true,
         index: true
     }
@@ -41,6 +42,30 @@ userSchema.pre("save", async function(next){
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id : this._id,
+        username : this.username,
+        email : this.email,
+        fullName : this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id : this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
 }
 
 export const User = mongoose.model("User", userSchema);
--- root/Backend/controllers/user.controller.js
+++ root/Backend/controllers/user.controller.js
@@ -18,6 +18,30 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 
 */
 
+
+// method for generating access and refresh token
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+
+        const user = await User.findById(userId);
+
+        const accessToken = await user.generateAccessToken()
+
+        const refreshToken = await user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        user.save({ validateBeforeSave : false})
+
+        return {accessToken, refreshToken}
+
+        
+    } catch (error) {
+        throw new ApiError(500, "Access and Refresh token generation failed")
+    }
+}
+
+
+
 const registerUser = asyncHandler( async (req, res) => {
 
     // get the user detials
@@ -85,9 +109,107 @@ const registerUser = asyncHandler( async (req, res) => {
 } )
 
 
+const loginUser = asyncHandler( async (req, res) => {
+
+    // Algorithm for LoginUser :
+
+    // step - 1 : get the username or email & password
+    // step - 2 : validate the inputs i.e. check for empty values & patterns 
+    // step - 3 : check in the database if the inputs match with the data in the database
+    //            if it matches proceed forward, if it not matches tell the user to 
+    //            register first
+    // step - 4 : if the user is matched successfully then generate access token & 
+    //            refresh token (store the refresh token in the db)
+    // step - 5 : if everything goes well then send the response with the access token &
+    //            refresh token and also set the cookies.
+
+
+    // get the user details
+    const {username, email, password} = req.body;
+
+
+    // validate the user inputs
+
+    // validation for empty values
+    if (
+        [username, email, password].some( (field) => field?.trim() === "" )
+    ) {
+        throw new ApiError(400, "you cannot provide empty fields")
+    }
+
+
+    // set email & username to lowercase
+    const lowerEmail = email.toLowerCase();
+    const lowerUsername = username.toLowerCase()
+
+
+    // Validate email pattern
+    const pattern = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}";
+    const emailToBeMatched = lowerEmail.match(pattern);
+    
+    if (emailToBeMatched === null) {
+        throw new ApiError(400, "Email pattern is Invalid")
+    }
+
+    const matchedEmail = emailToBeMatched[0]
+
+    // find the user in the database 
+    const existingUser = User.findOne({
+        $or : [{lowerUsername},{matchedEmail}]
+    })
+
+    if(!existingUser){
+        throw new ApiError(400, "The user does not exists, Please register yourself first !")
+    }
+
+
+    // if user exists already, check the provided password
+    const passwordCheck = existingUser.isPasswordCorrect(password);
+
+    if(!passwordCheck){
+        throw new ApiError(401, "Password is incorrect !")
+    }
+
+
+
+    // generate accessToken & refreshToken 
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(existingUser._id)
+
+    // the token generation method has already stored the refresh token in the database so no need to do it here again 
+
+    // send cookie
+
+    // first bring the loggedIn user from db
+    const loggedInUser = await User.findById(existingUser._id).select("-password, -refreshToken")
+
+    cookieOptions = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .cookie("AccessToken", accessToken, cookieOptions)
+    .cookie("RefreshToken", refreshToken, cookieOptions)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user : loggedInUser, 
+                accessToken, 
+                refreshToken
+            },
+            "User Logged-in successfully"
+        )
+    );
+    
+});
 
 
 
 
 // export all user methods
-export {registerUser}
+export {
+    registerUser,
+    loginUser,
+}
