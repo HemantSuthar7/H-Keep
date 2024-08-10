9a1e5d225fd0260e6b6f1a7c3bc0db4949b587e3
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-08-10 18:49:10 +0530

    implemented get labelData method

--- root/Backend/src/controllers/label.controllers.js
+++ root/Backend/src/controllers/label.controllers.js
@@ -2,6 +2,9 @@ import {asyncHandler} from "../utils/ascyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import { Label } from "../Models/Label.models.js"
+import { Note } from "../Models/Note.models.js"
+import {TodoList} from "../Models/TodoList.models.js"
+import mongoose from "mongoose"
 
 /*
 
@@ -77,6 +80,81 @@ const createLabel = asyncHandler( async (req, res) => {
 
 
 
+const getLabelData = asyncHandler( async (req, res) => {
+
+    // logic for getting all the label associated data 
+    //      1. get the labelId and userId
+    //      2. get all the notes and lists where label category is the provided labelId
+    //      3. return the notes and lists differently as each one as an array of objects
+    //      4. if there is no data associated with label then also return notes and lists as empty arrays
+
+    const { labelId } = req.params;
+
+    const userId = req.user._id;
+
+
+    if(!userId){
+        throw new ApiError(400, "The user is not authenticated for retrieving labelData")
+    }
+
+
+    if (!labelId || typeof labelId !== "string" || labelId.trim() === "") {
+        throw new ApiError(400, "Invalid labelId passed");
+    }
+
+    const existingLabel = await Label.findById(labelId);
+
+    if(!existingLabel){
+        throw new ApiError(404, "Label not found, invalid label-id")
+    }
+
+
+    const labelData = await Promise.all([
+        Note.aggregate([
+            {
+                $match : {
+                    labelCategory : mongoose.Types.ObjectId(labelId),
+                    createdBy : mongoose.Types.ObjectId(userId)
+                }
+            }
+        ]),
+        TodoList.aggregate([
+            {
+                $match : {
+                    labelCategory : mongoose.Types.ObjectId(labelId),
+                    createdBy : mongoose.Types.ObjectId(userId)
+                }
+            }
+        ])
+    ])
+
+
+    const [notes, todoLists] = labelData;
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                notes : notes || [],
+                todoLists : todoLists || []
+            },
+            "Label data retrieved successfully"
+        )
+    )
+
+
+} )
+
+
+const updateLabel = asyncHandler( async (req, res) => {
+    
+} )
+
+
 export {
     createLabel,
+    getLabelData
 }
