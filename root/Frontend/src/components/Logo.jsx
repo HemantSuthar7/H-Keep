acfe877bbcdf4f4776bf8c581956310e99c7d2fe
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-08-29 16:31:24 +0530

    added few basic components as well as made the store with authSlice

---
+++ root/Frontend/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({ width = "100px" }) {
+  return (
+    <img src="\src\assets\H-Keep.png" alt="H-Blog" className='rounded-md' />
+  )
+}
+
+export default Logo
