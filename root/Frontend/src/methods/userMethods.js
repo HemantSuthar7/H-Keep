afff6ca842aaebd9dcdb24126b11716a16ccbcaf
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-08-29 21:08:36 +0530

    added some more methods in the userMethods

--- root/Frontend/src/methods/userMethods.js
+++ root/Frontend/src/methods/userMethods.js
@@ -14,13 +14,6 @@ import axios from "../api/axiosConfig.js"
 
 const registerUser = async ({username, password, email, fullName}) => {
     try {
-        
-        // somehow communicate with the backend via the url and return the response.
-        // If the backend returns an error then it should be caugth in the catch block.
-        // the url is : http://localhost:5000/api/v1/users/register
-        // Remember this is a POST request.
-        // we may use the axios or fetch to perform this .
-
         const response = await axios.post('http://localhost:5000/api/v1/users/register', {
             username,
             password,
@@ -34,4 +27,68 @@ const registerUser = async ({username, password, email, fullName}) => {
     } catch (error) {
         console.log("Error from Backend : Register-User Error : Error : ", error)
     }
+}
+
+
+
+const loginUser = async ({username, email, password}) => {
+    try {
+        
+        const response = await axios.post("http://localhost:5000/api/v1/users/login" , {
+            username, 
+            email, 
+            password
+        })
+
+        return response.data
+
+    } catch (error) {
+        console.log("Error from Backend : Login-User Error : Error : ", error)
+    }
+}
+
+
+const logoutUser = async () => {
+    try {
+
+        const response = await axios.post("http://localhost:5000/api/v1/users/logout" , {
+            username, 
+            email, 
+            password
+        })
+
+        return response.data
+        
+    } catch (error) {
+        console.log("Error from Backend : Logout-User Error : Error : ", error)
+    }
+}
+
+
+const refreshAccessToken = async () => {
+
+}
+
+
+const changeCurrentPassword = async ({oldPassword, newPassword}) => {
+    try {
+        
+        const response = await axios.post("http://localhost:5000/api/v1/users/change-password" , {
+           oldPassword,
+           newPassword
+        })
+
+        return response.data;
+
+    } catch (error) {
+        console.log("Error from Backend : Change-Current-Password Error : Error : ", error)
+    }
+}
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken, // i have some doubts over the refresh token and access token transfers so any method related to tokens will be completed later on...
+    changeCurrentPassword,
 }
