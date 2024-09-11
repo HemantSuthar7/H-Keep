0ae8e73dfefff14d7c848bef966ff269db59eed0
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-09-11 12:43:14 +0530

    changed the assets to public

--- root/Frontend/src/components/Logo.jsx
+++ root/Frontend/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({ width = "100px" }) {
   return (
-    <img src="\src\assets\H-Keep.png" alt="H-KEEP" className='rounded-md w-24 shadow-lg shadow-black bg-black' />
+    <img src="\assets\H-Keep.png" alt="H-KEEP" className='rounded-md w-24 shadow-lg shadow-black bg-black' />
   )
 }
 
