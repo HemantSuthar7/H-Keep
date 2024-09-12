86772e6eb4d5fa20e4f38a092eab94c111b9dc6e
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-09-12 21:28:01 +0530

    changed the url

--- root/Frontend/src/api/axiosConfig.js
+++ root/Frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://h-keep-backend.onrender/api/v1',
+  baseURL: 'https://h-keep-backend.onrender.com/api/v1',
   withCredentials: true,
   timeout: 10000,
   headers: {},
